d95163bb9b6269bcf7ad0b1b70928ea949ebe9cd
Minyeong Jeong <megormin@gmail.com>
2024-11-13 21:11:58 +0100

    refactored and styled

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import {
   IPokemonType,
 } from "./interface/IPokeAPI";
 
-// ^=============================================================================
+// ^==========================================================
 
 const inputText = document.querySelector("#inputText") as HTMLInputElement;
 const buttons = document.querySelectorAll(
@@ -14,11 +14,11 @@ const buttons = document.querySelectorAll(
 ) as NodeListOf<HTMLButtonElement>;
 const cardsWrapper = document.querySelector("#cardsWrapper") as HTMLDivElement;
 
-// ^=============================================================================
+// ^==========================================================
 
 const BASE_URL = "https://pokeapi.co/api/v2/";
 
-// ^=============================================================================
+// ^==========================================================
 
 const fetchPokemons = async (buttonValue: string) => {
   cardsWrapper.innerHTML = "";
@@ -29,47 +29,59 @@ const fetchPokemons = async (buttonValue: string) => {
   const pokemonArr: IPokemonPokemon[] = data.pokemon;
 
   pokemonArr.forEach((singlePokemon: IPokemonPokemon) => {
-    renderCards(singlePokemon);
+    renderCards(singlePokemon.pokemon, true);
   });
 };
-// ^=============================================================================
+// ^==========================================================
+
 buttons?.forEach((button: HTMLButtonElement) => {
   button?.addEventListener("click", () => {
     const buttonValue = button.value;
-    console.log(buttonValue);
+
     fetchPokemons(buttonValue);
   });
 });
-// ^=============================================================================
-function renderCards(singlePokemon: IPokemonPokemon) {
+
+// ^==========================================================
+
+function renderCards(pokemon: IPokemon, styleType: boolean) {
   const cardDiv = document.createElement("div") as HTMLDivElement;
   const cardImg = document.createElement("img") as HTMLImageElement;
   const cardId = document.createElement("p") as HTMLParagraphElement;
   const cardName = document.createElement("p") as HTMLParagraphElement;
-  // *=============================================================
-  const id = singlePokemon.pokemon.url
+  // ========================================================
+  const id = pokemon.url
     .replace("https://pokeapi.co/api/v2/pokemon/", "")
     .replace("/", "");
   cardImg.setAttribute(
     "src",
-    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
   );
-  cardId.textContent = id;
-  cardName.textContent = singlePokemon.pokemon.name;
-  // *=============================================================
+  cardId.textContent = "#" + id.padStart(3, "0");
+  cardName.textContent = pokemon.name;
+  // ========================================================
+  if (styleType === true) {
+    cardDiv.classList.add("cardDiv", "flex");
+  } else if (styleType === false) {
+    cardDiv.classList.add("flex", "pokedex");
+  }
+
+  // ========================================================
   cardDiv.append(cardImg, cardId, cardName);
   cardsWrapper.appendChild(cardDiv);
 }
-// ^=============================================================================
+// ^==========================================================
+
 inputText.addEventListener("keyup", ({ key }) => {
   if (key === "Enter") {
     const textValue = inputText.value.trim().toLowerCase();
-    console.log(textValue);
+
     fetchPokemonArr(textValue);
   }
 });
 
 const fetchPokemonArr = async (textValue: string) => {
+  cardsWrapper.innerHTML = "";
   const allPokemonURL = `${BASE_URL}pokemon?limit=1000&offset=0`;
   const response = await fetch(allPokemonURL);
   const data: IPokeAPI = await response.json();
@@ -80,22 +92,12 @@ const fetchPokemonArr = async (textValue: string) => {
       return singlePokemon.name === textValue;
     }
   );
+  console.log(filteredPokemonArr);
 
-  const cardDiv = document.createElement("div") as HTMLDivElement;
-  const cardImg = document.createElement("img") as HTMLImageElement;
-  const cardId = document.createElement("p") as HTMLParagraphElement;
-  const cardName = document.createElement("p") as HTMLParagraphElement;
-  // *=============================================================
-  const id = filteredPokemonArr[0].url
-    .replace("https://pokeapi.co/api/v2/pokemon/", "")
-    .replace("/", "");
-  cardImg.setAttribute(
-    "src",
-    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`
-  );
-  cardId.textContent = id;
-  cardName.textContent = filteredPokemonArr[0].name;
-  // *=============================================================
-  cardDiv.append(cardImg, cardId, cardName);
-  cardsWrapper.appendChild(cardDiv);
+  renderCards(filteredPokemonArr[0], false);
 };
+
+// const fetchPokeomeType = async (url: string) => {
+//   const response = await fetch(url);
+//   const data = await response.json();
+// };
